9245e1c92445fb9bbdd7fe2f017614a01e0c2456
AlexKMarshall <alexmarshall@qogita.com>
2023-05-07 09:11:15 +0100

    test: add pdp to lighthouse ci

--- apps/shop/lighthouserc.cjs
+++ apps/shop/lighthouserc.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3000/', 'http://localhost:3000/headphones'],
+      url: [
+        'http://localhost:3000/',
+        'http://localhost:3000/headphones',
+        'http://localhost:3000/product/xx99-mark-ii-headphones',
+      ],
       startServerCommand: 'pnpm start',
     },
     upload: {
