1146fed6cf4573d982d981d6392824230671a0dc
AlexKMarshall <alexmarshall@qogita.com>
2023-05-18 16:08:25 +0100

    build: add kv variables to schema

--- apps/shop/src/env.mjs
+++ apps/shop/src/env.mjs
@@ -10,6 +10,10 @@ export const env = createEnv({
     NODE_ENV: z.enum(['development', 'test', 'production']),
     SANITY_PROJECT_ID: z.string().min(1),
     SANITY_DATASET: z.string().min(1),
+    KV_URL: z.string().min(1).url(),
+    KV_REST_API_URL: z.string().min(1).url(),
+    KV_REST_API_TOKEN: z.string().min(1),
+    KV_REST_API_READ_ONLY_TOKEN: z.string().min(1),
   },
 
   /**
@@ -29,6 +33,10 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
     SANITY_DATASET: process.env.SANITY_DATASET,
+    KV_URL: process.env.KV_URL,
+    KV_REST_API_URL: process.env.KV_REST_API_URL,
+    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+    KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
 })
